1f09408fb5102ac8d937513813901204a385f070
pmaitland <pmaitland98@gmail.com>
2026-01-31 16:07:17 +0000

    grid places towers

--- mask/scripts/cell.gd
+++ mask/scripts/cell.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 
+func disable() -> void:
+	AStar.set_disabled(AStar.global_to_cell(global_position), true)
+	visible = !AStar.is_disabled(AStar.global_to_cell(global_position))
+
+
 func _on_area_2d_input_event(_viewport: Node, _event: InputEvent, _shape_idx: int) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		AStar.set_disabled(AStar.global_to_cell(global_position), true)
-		visible = !AStar.is_disabled(AStar.global_to_cell(global_position))
+		disable()

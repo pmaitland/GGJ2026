0f086e9c2426388f82c63a9bcc73c31187d24833
pmaitland <33162414+pmaitland@users.noreply.github.com>
2026-01-31 16:17:59 +0000

    grid places towers (#12)

--- mask/scripts/grid.gd
+++ mask/scripts/grid.gd
@@ -6,11 +6,15 @@ extends Node
 @export var start: Vector2i = Vector2i(0, 0)
 @export var end: Vector2i = Vector2i(9, 9)
 @export var ant_count: int = 10
+@export var spray_bottles: Array[Vector2i]
 
 var ant_scene: PackedScene
+
 var ant_spawn_timer: Timer
 var ant_spawn_cooldown: float = 2
 var ants_spawned_count: int = 0
+
+var cells: Array[Node2D] = []
 
 
 func _ready() -> void:
@@ -24,7 +28,9 @@ func _ready() -> void:
 		for x in AStar.width:
 			var cell: Node2D = cell_scene.instantiate()
 			cell.global_position = AStar.cell_to_global(Vector2(x, y))
+			cells.append(cell)
 			add_child(cell)
+	_create_spray_bottles()
 
 
 func _process(_delta) -> void:
@@ -34,3 +40,14 @@ func _process(_delta) -> void:
 		add_child(ant)
 		ants_spawned_count += 1
 		ant_spawn_timer.start(ant_spawn_cooldown)
+
+
+func _create_spray_bottles() -> void:
+	var spray_bottle_scene: PackedScene = load("res://scenes/tower.tscn")
+	for coords in spray_bottles:
+		var spray_bottle: Node2D = spray_bottle_scene.instantiate()
+		spray_bottle.position = AStar.cell_to_global(coords)
+		add_child(spray_bottle)
+		for cell in cells:
+			if cell.position == spray_bottle.position:
+				cell.disable()

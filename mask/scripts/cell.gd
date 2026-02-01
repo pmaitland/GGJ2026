c28e851a1e66ac069bb31d5a537b640a76930757
pmaitland <pmaitland98@gmail.com>
2026-02-01 10:14:37 +0000

    cell outline

--- mask/scripts/cell.gd
+++ mask/scripts/cell.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 
+@onready var outline: Sprite2D = $Sprite2D
+
+
 func disable() -> void:
 	AStar.set_disabled(AStar.global_to_cell(global_position), true)
 	if disabled():
@@ -14,3 +17,11 @@ func disabled() -> bool:
 func _on_area_2d_input_event(_viewport: Node, _event: InputEvent, _shape_idx: int) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		disable()
+
+
+func _on_area_2d_mouse_entered() -> void:
+	outline.visible = true
+
+
+func _on_area_2d_mouse_exited() -> void:
+	outline.visible = false

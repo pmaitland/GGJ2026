7b2cb7eb956b474cf02f1ba97cc8d6ba2c1e9a88
Iain Cattermole <iaincattermole@skyscanner.net>
2026-02-01 15:36:38 +0000

    dead ants

--- mask/scripts/grid.gd
+++ mask/scripts/grid.gd
@@ -77,7 +77,7 @@ func _next_ant() -> PackedScene:
 
 func is_ants_remaining() -> bool:
 	for child in get_children():
-		if child is Ant or child.get_child(0) is Ant:
+		if child is Ant or child.name.to_lower().contains("ant"):
 			return true
 	return false
 
